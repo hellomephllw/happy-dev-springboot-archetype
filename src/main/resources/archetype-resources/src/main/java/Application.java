7b91a657f21025b0330c54d3c9b20c212e3a5322
liliwen <hellomephllw@163.com>
2021-05-19 01:04:35 +0800

    初始化

---
+++ src/main/resources/archetype-resources/src/main/java/Application.java
@@ -0,0 +1,19 @@
+package ${package};
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@ServletComponentScan
+@EnableScheduling
+@MapperScan("${package}.dao")
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/resources/archetype-resources/src/test/java/Test.java
@@ -0,0 +1,9 @@
+package ${package};
+
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+
+}
