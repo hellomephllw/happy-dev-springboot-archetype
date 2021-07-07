48fa01f8b6a3c6147f03609559afe66480944582
liliwen <hellomephllw@163.com>
2021-07-07 15:23:23 +0800

    添加mybatis插件配置

---
+++ src/main/resources/archetype-resources/src/main/java/init/MyBatisPluginsConfig.java
@@ -0,0 +1,15 @@
+package ${package}.init;
+
+import com.happy.express.paging.plugin.EasyPagingPlugin;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * @description: MyBatis插件配置
+ * @author: liliwen
+ * @date: 2021-07-06
+ */
+@Configuration
+@Import(EasyPagingPlugin.class)
+public class MybatisPluginConfig {
+}
